1d55dfe7d00ae9d5086ed69ee7f942ee61830075
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-01-03 18:20:40 -0700

    copy to clipboard added

--- script.js
+++ script.js
@@ -5,6 +5,63 @@ var generateBtn = document.querySelector(".generate");
 var copyBtn = document.querySelector(".copy");
 var pasEl = document.querySelector(".password-container");
 
+var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+var numChar = "1234567890";
+
+function generateSelectedCharacterOptions(cap, low, special, num) {
+    var characterOptions = [];
+        
+    if(cap) {
+        characterOptions.push(capLetters);
+    }
+    if(low) {
+        characterOptions.push(lowerLetters);
+    }
+    if(special) {
+        characterOptions.push(specialChar);
+    }
+    if(num) {
+        characterOptions.push(numChar);
+    }
+}
+
+function randVal(length) {
+
+}
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    textArea.style.position="fixed";  //avoid scrolling to bottom
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+}
+
 generateBtn.addEventListener("click", function(event) {
     //character length prompt
     var pasLength = prompt("How long would you like your password to be? (between 8 and 128 characters)");
@@ -20,29 +77,28 @@ generateBtn.addEventListener("click", function(event) {
     var low = confirm("Would you like lower case letters?");
     var special = confirm("Would you like special characters? (!#$%&()*+,-./:;<=>?@[\]^_`{|}~)");
     var num = confirm("Would you like numbers?");
-    var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-    var specialChar = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-    var numChar = 1234567890;
-
-    var capital = cap[true] + capLetters;
-    var lowerCase = low[true] + lowerLetters;
-    var specChar = special[true] + specialChar;
-    var numbers = num[true] + numChar;
     
     if (cap === false && low === false && special === false && num === false) {
         alert("I'm sorry, you have to choose at least one character, please try again.");
-        }
-    else {
-        var characters = capital + lowerCase + specChar + numbers;
-        pasEl.textContent = password(pasLength.value, characters);
-
-        function password(l){
-            var pwd = '';
-            for(var i = 0; i<l; i++){
-                pwd = characters.charAt(Math.floor(Math.random() * characters.length));
-            }
-            console.log(pwd);
-        }
     }
+    else {
+        //shape "" ""
+        characterOpts = generateSelectedCharacterOptions(cap, low, special, num);
+        var password = ""
+        //loop and randomly choose characters based on users specifications for paslength 
+        for (var i = 0; i <= pasLength + 1; i++){
+            // var randOptions =  
+            // var randChar = 
+            password +=characterOpts[Math.floor(Math.random() * characterOpts.length)]
+            //randomly choose a type of character
+            //we need to randomly choose a character from that type
+            //we apply that character to current password
+        }
+        //supply password to user
+        pasEl.textContent = password;
+    }
+});
+
+copyBtn.addEventListener('click', function(event) {
+    copyTextToClipboard(pasEl.textContent);
 });
